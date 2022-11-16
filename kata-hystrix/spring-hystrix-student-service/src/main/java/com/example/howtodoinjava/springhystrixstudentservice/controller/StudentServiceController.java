2926f14df39056c9940da9893665bb99f028c19c
Owen <lebec.owen@yahoo.fr>
2022-11-16 16:44:53 +0100

    Ok

---
+++ kata-hystrix/spring-hystrix-student-service/src/main/java/com/example/howtodoinjava/springhystrixstudentservice/controller/StudentServiceController.java
@@ -0,0 +1,12 @@
+package com.example.howtodoinjava.springhystrixstudentservice.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StudentServiceController {
+
+    // TODO create DB student
+
+    // TODO get student details for specific school and expose endpoint
+
+}
---
+++ kata-hystrix/spring-hystrix-student-service/src/main/java/com/example/howtodoinjava/springhystrixstudentservice/domain/Student.java
@@ -0,0 +1,6 @@
+package com.example.howtodoinjava.springhystrixstudentservice.domain;
+
+public class Student {
+    // TODO create student with name, className
+
+}
